834cd92f923cd890bae3a870af983f2e054763c4
Mehdi Lalaoui <lalaouimehdi7@gmail.com>
2020-09-25 15:41:16 +0200

    fix connection to backend

--- src/api/socket_methods.ts
+++ src/api/socket_methods.ts
@@ -1,5 +1,6 @@
 import io from 'socket.io-client';
 
 export function connect(): SocketIOClient.Socket{
+    console.log('coucou');
     return io.connect(process.env.REACT_APP_BACKEND as string)
 }
